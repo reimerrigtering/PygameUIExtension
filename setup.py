5206d183660d1e5b33a14053a7a21c1c7076d18b
reimerrigtering <reimer.rigtering@gmail.com>
2024-06-13 14:35:19 +0200

    created setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='PygameUIExtension',
+    version='2.0.1',
+    author='Reimer Rigtering',
+    author_email='reimer.rigtering@gmail.com',
+    url='https://github.com/reimerrigtering/PygameUIExtension',
+    requires=['pygame']
+)
